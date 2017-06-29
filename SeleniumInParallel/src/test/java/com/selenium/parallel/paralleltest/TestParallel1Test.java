1bb30e07d78544691e93764f400359d08470409b
jyothishkumarav <jyothishkumarav@gmail.com>
2017-06-29 13:07:01 -0400

    Adding multiple Tests to class

--- SeleniumInParallel/src/test/java/com/selenium/parallel/paralleltest/TestParallel1Test.java
+++ SeleniumInParallel/src/test/java/com/selenium/parallel/paralleltest/TestParallel1Test.java
@@ -1,6 +1,12 @@
 package com.selenium.parallel.paralleltest;
 
+import java.util.List;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
@@ -26,8 +32,24 @@ public class TestParallel1Test {
 		driver.get("https://facebook.com");
 		System.out.println("Page Title is :" + driver.getTitle());
 	}
+	
+	@Test
+	public void testUserName(){
+		
+		List<WebElement> userLabels = driver.findElements(By.cssSelector(".html7magic>label"));
+		String userNameTxt = userLabels.get(0).getText();
+		Assert.assertEquals(userNameTxt, "Email or Phone");
+	}
+	
+	@Test
+	public void testTitle(){
+		
+		String title = driver.getTitle();
+		Assert.assertEquals(true, title.contains("Facebook"));
+		
+	}
 
-	@AfterTest
+	@AfterClass
 	public void endTest() {
 		//driver.quit();
 		// driver.close();
